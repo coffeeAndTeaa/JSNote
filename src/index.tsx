615cfaa663adb90ce300d0952374df064dff5412
Allen <AllenWithJS@gmail.com>
2023-01-16 18:42:51 -0500

    switch to react18 more sanity check

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,31 @@
-import ReactDOM from "react-dom";
-import React from "react";
+import React,{useState, useRef} from "react";
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container!);
 
 const App = () => {
-  return <h1>Hello world</h1>
+  const [input, setInput] = useState<string>("");
+  const [code, setCode] = useState<string>("");
+
+  const handleTextChange = (e: any) => {
+    setInput(e.target.value);
+  }
+
+  const handleClick = () => {
+    console.log(input);
+  }
+
+  return (<>
+    <textarea onChange={handleTextChange} value={input}>
+    </textarea>
+    <div>
+      <button onClick={handleClick}>submit</button>
+    </div>
+    {// where the code will show up
+    }
+    <pre></pre>
+  </>);
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+root.render(<App/>);
+
