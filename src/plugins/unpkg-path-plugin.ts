ad3c157683f55dac5a31e9eb1acb1329b51872c0
Allen <AllenWithJS@gmail.com>
2023-01-16 20:01:53 -0500

    为plugin添加了一些注释，说明了 resolve和onLoad的逻辑

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -6,12 +6,6 @@ const fileCache = localforage.createInstance({
   name: "filecache"
 });
 
-(async () => {
-  await fileCache.setItem("color", "blue");
-
-  const color = await fileCache.getItem("color");
-  console.log(color);
-})();
 
 export const unpkgPathPlugin = (inputCode: string) => {
   return {
@@ -24,16 +18,18 @@ export const unpkgPathPlugin = (inputCode: string) => {
           namespace: "a"
         };
       })
-
       
-
-
+//===========================================
+//============onResolve: 负责返回文件所在的路径名
+//===========================================
       build.onResolve({ filter: /.*/ }, async (args: any) => {
         console.log('onResolve', args);
         if (args.path === 'index.js') {
           return { path: args.path, namespace: 'a' };
         }
 
+        // 处理从相对路径引入文件的情况
+        // args.resolveDir表示当前的子目录
         if (args.path.includes('./') || args.path.includes('../')) {
           return {
             namespace: 'a',
@@ -43,12 +39,19 @@ export const unpkgPathPlugin = (inputCode: string) => {
             ).href,
           };
         }
-
+        
+        // 处理最一般的情况例如 import React from "react"
         return {
           namespace: 'a',
+          // 自动生成url
           path: `https://unpkg.com/${args.path}`,
         };
       });
+
+
+//===================================================================
+//===onLoad: 负责返回路径对应文件的内容， loader 提醒esbuild 可能需要解析JSX
+//===================================================================
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
         console.log('onLoad', args);
