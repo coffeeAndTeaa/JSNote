7e0b31010acbe09433ee8108ac212fb9209092d0
Allen <AllenWithJS@gmail.com>
2023-01-19 18:53:27 -0500

    添加了store和reducer

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,17 @@ import { createRoot } from "react-dom/client";
 import "bulmaswatch/darkly/bulmaswatch.min.css";
 import CodeCell from "./components/code-cell";
 import TextEditor from "./components/text-editor";
+import { Provider } from "react-redux";
+import { store } from "./state";
 // import 'bulmaswatch/superhero/bulmaswatch.min.css';
 
 const App = () => {
   return (
-    <div>
-      <TextEditor />
-    </div>
+    <Provider store={store}>
+      <div>
+        <TextEditor />
+      </div>
+    </Provider>
   );
 };
 
